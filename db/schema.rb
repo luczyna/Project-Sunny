921b035e3904c5c7457087d4255b81e58865ed2a
Ana Lu <luczynaki@gmail.com>
2017-06-01 00:12:12 -0500

    index user emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601042805) do
+ActiveRecord::Schema.define(version: 20170601050022) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170601042805) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "{:foreign_key=>true}_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["{:foreign_key=>true}_id"], name: "index_users_on_{:foreign_key=>true}_id"
   end
 
