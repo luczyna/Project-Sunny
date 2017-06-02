6202e9f3f93e4d42b4ab017753aafd1645b96105
Ana Lu <luczynaki@gmail.com>
2017-06-02 14:43:16 -0500

    make form for new ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,16 +5,39 @@ class IdeasController < ApplicationController
   end
 
   def show
+    @warning = false
+    @idea = Idea.find_by_id(params[:id])
+
+    if @idea && @idea.published
+      @version = Version.where(idea_id: @idea.id).last
+    else
+      @warning = true
+    end
   end
 
   def edit
   end
 
   def new
+    # TODO how to make two submit buttons? save and publish, just save
+    # TODO how to disable a checkbox tag?
+    @too_many_published = Idea.where(user_id: current_user.id, published: true).count == 5
   end
 
   # POST
   def create
+    idea = Idea.new(name: params[:name], user_id: current_user.id)
+    
+    if idea.save
+      version = Version.new(idea_id: idea.id, goal: params[:goal], content: params[:content], number: 1)
+
+      if version.save
+        redirect_to idea
+      else
+      end
+    else
+      # TODO show dem errors
+    end
   end
 
   # PATCH/PUT
