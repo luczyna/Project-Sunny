86cc7f5fc9da711295ffb0c063185c853728ce60
Ana Lu <luczynaki@gmail.com>
2017-06-02 13:56:02 -0500

    update models and add tests for new ideas

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,12 +1,14 @@
 class Version < ApplicationRecord
   belongs_to :idea
 
-  # validates_uniqueness_of :number, scope => :idea
-  # validates :number, uniqueness: { scope: :idea }
+  validates :idea_id, presence: true
+  validates :number, presence: true, uniqueness: { scope: :idea }
   
-  validates_presence_of :content, message: "We need an idea description."
-  validates_length_of :content, maximum: 500, message: "Please shorten your idea description."
+  validates :content, 
+    presence: { message: "We need an idea description." },
+    length: { maximum: 500, message: "Please shorten your idea description." }
 
-  validates_presence_of :goal, message: "We need a summary of this idea."
-  validates_length_of :goal, maximum: 75, message: "Please shorten this short pitch."
+  validates :goal,
+    presence: { message: "We need a summary of this idea."},
+    length: { maximum: 75, message: "Please shorten this short pitch."}
 end
