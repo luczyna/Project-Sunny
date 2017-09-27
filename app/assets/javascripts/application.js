c9e716c45af114cf56178fc2d6d69f43d835f6df
Ana Lu <luczynaki@gmail.com>
2017-09-27 13:50:09 -0500

    add bootstrap to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
 
 window.addEventListener('load', cleanUpWindowReady, false);
