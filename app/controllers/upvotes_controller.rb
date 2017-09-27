3dfe4816c90da36d657558e72977a7797985a581
Ana Lu <luczynaki@gmail.com>
2017-09-27 13:11:09 -0500

    allow a user to update their vote to the current version of an idea

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -25,4 +25,21 @@ class UpvotesController < ApplicationController
       # TODO show errors
     end
   end
+
+  def update
+    # remove the user's vote (provided) from the previous version
+    # add the vote to the current version (provided)
+    current_version = Version.find(params[:version])
+    idea = current_version.idea
+
+    # TODO think on how to direct errors?
+    Upvote.find(params[:vote]).destroy
+    vote = Upvote.new(user_id: current_user.id, version_id: current_version.id)
+
+    if vote.save
+      redirect_to idea
+    else
+      # TODO more error handling
+    end
+  end
 end
