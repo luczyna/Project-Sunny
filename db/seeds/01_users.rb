2afae00109be7db0ee97c81e95700924308066c2
Ana Lu <luczynaki@gmail.com>
2017-09-28 20:49:58 -0500

    create more ideas and users with seed
    * employ separate files for seed stages
    * add a new test user

---
+++ db/seeds/01_users.rb
@@ -0,0 +1,5 @@
+# Create Users that we'll reference everywhere
+
+User.create(username: "ana", email: "asd@asd.com", password: "poi")
+User.create(username: "bana", email: "wer@wer.com", password: "poi")
+User.create(username: "lana", email: "ert@ert.com", password: "poi")
